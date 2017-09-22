18ec048c2a0d316afe2adb307070ca36beaea1c0
Rishabjaincode <rishab.jain.code@gmail.com>
2017-09-22 13:17:42 +0530

    Create socialtweepy.py

---
+++ socialtweepy.py
@@ -0,0 +1,25 @@
+from tweepy.streaming import Stream
+from tweepy.auth import OAuthHandler
+from tweepy.streaming import StreamListener
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey="Ht2hjyNon1BgyMxhBhbC2reSF"
+csecret="1REwJr4uTdTVKVlMTmbeNBPDDEJJuQspdZD7kpc0f7U4AkQHI4"
+atoken="738527443-xXzYkJ8SEIZuCI4dCAoPLmV6rwi8joLDR226BirC"
+asecret="fD8JdWcNaO3TFiUQMQm5yrFkHWGDEJpXBmpnAYccS4lpM"
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return(True)
+
+    def on_error(self, status):
+        print status
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["car"])
